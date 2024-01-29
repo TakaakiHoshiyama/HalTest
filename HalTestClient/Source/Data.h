d2c0dbd57d86ba1f19a0078dea58eb8a6c414a0b
34WNINT037\game <horikawahikaru1@gmail.com>
2024-01-29 20:25:47 +0900

    好物を焼肉に変更

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -2,5 +2,5 @@ namespace Hoshiyama
 {
 	const char* TextMessage1 = "私の名前は次郎です。";
 	const char* TextMessage2 = "私の年齢は20歳です。";
-	const char* TextMessage3 = "私の好きな食べ物はラーメンです。";
+	const char* TextMessage3 = "私の好きな食べ物は焼肉です。";
 }
