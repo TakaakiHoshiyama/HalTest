c70f1b25b18a006c4bf26b7cd35169bd10199667
K.Nishida <Kazuki.Nishida888@gmail.com>
2024-01-31 20:20:45 +0900

    名前を変更

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,6 +1,6 @@
 namespace Hoshiyama
 {
-	const char* TextMessage1 = "私の名前は金太郎です。";
+	const char* TextMessage1 = "私の名前は五郎です。";
 	const char* TextMessage2 = "私の年齢は30歳です。";
 	const char* TextMessage3 = "私の好きな食べ物はカレーライスです。";
 }
