cdc2f2f78ec54851db0d2d8d3223c3c49b495de1
shonda <honshin.0411@gmail.com>
2024-01-31 20:40:21 +0900

    名前を田中に変更

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,9 +1,7 @@
 namespace Hoshiyama
 {
-<<<<<<< HEAD
-	const char* TextMessage1 = "私の名前は次郎です。";
+	const char* TextMessage1 = "私の名前は田中です。";
 	const char* TextMessage2 = "私の年齢は20歳です。";
 	const char* TextMessage3 = "私の好きな食べ物は焼肉です。";
 	const char* TextMessage4 = "私の性別は男性です。";
->>>>>>> kokke1_29
 }
