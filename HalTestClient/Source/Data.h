28a9c4eb6f4b2548a3e66f71fa8996049ead2900
shonda <honshin.0411@gmail.com>
2024-01-31 19:48:01 +0900

    性別の情報を追加

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -3,4 +3,5 @@ namespace Hoshiyama
 	const char* TextMessage1 = "私の名前は土佐万次郎です。";
 	const char* TextMessage2 = "私の年齢は38歳です。";
 	const char* TextMessage3 = "私の好きな食べ物はうな重です。";
+	const char* TextMessage4 = "私の性別は男性です。";
 }
