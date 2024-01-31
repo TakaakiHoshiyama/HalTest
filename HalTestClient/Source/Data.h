c89fc3273d51129f4d733093cf055e8028d611d6
skousuke <k466@ymail.ne.jp>
2024-01-31 20:43:21 +0900

    年齢を漢字へ変更

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,7 +1,7 @@
 namespace Hoshiyama
 {
 	const char* TextMessage1 = "私の名前は太郎です。";
-	const char* TextMessage2 = "私の年齢は20歳です。";
+	const char* TextMessage2 = "私は二十歳です。";
 	const char* TextMessage3 = "私の好きな食べ物は焼肉です。";
 }
 
