2eb58e3a7a8fb825a8daadc5f892d44cffd8fe01
hal_zhecheng <yagamizc@gmail.com>
2024-05-08 20:55:07 +0900

    テストコメント入れました

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -3,4 +3,5 @@ namespace Hoshiyama
 	const char* TextMessage1 = "私の名前は三郎です。";
 	const char* TextMessage2 = "私の年齢は35歳です。";
 	const char* TextMessage3 = "私の好きなゲームはFFです。";
+	const char* TextMessage4 = "testtesttest";
 }
