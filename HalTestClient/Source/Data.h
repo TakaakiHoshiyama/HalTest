890b1faec77d1f56e73b681ff7730b6a2d1a8051
sue <aaaa>
2024-05-13 20:22:29 +0900

    Revert "Merge branch 'sue_test' into develop"
    This reverts commit a524037cc345e473a6ce3e0a7d0fd917df7c48cb, reversing
    changes made to 0c16095b4c656de43a9cd814bf3bc0504a24facb.

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,16 +1,7 @@
 namespace Hoshiyama
 {
 	const char* TextMessage1 = "私の名前は三郎です。";
-<<<<<<< HEAD
 	const char* TextMessage2 = "私の年齢は20歳です。";
 	const char* TextMessage3 = "私の好きなゲームは　　です。";
 	const char* TextMessage5 = "私の好きな食べ物はチーズです。";
-=======
-	const char* TextMessage2 = "私の年齢は35歳です。";
-	const char* TextMessage5 = "私の好きな食べ物はラーメンです。";
-	const char* TextMessage5 = "北海道なら蔵生サブレです。";
-	const char* TextMessage5 = "宮城ならずんだ＆あずきラングドシャです。";
-
->>>>>>> sue_test
 }
-
