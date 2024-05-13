7f11c29c02f23626d613c6585863ad569d9fa363
TOKYO-NET\hoshiyama.takaaki <hoshiyama@pickle.ne.jp>
2024-05-13 19:33:46 +0900

    年齢を変えました

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,6 +1,6 @@
 namespace Hoshiyama
 {
 	const char* TextMessage1 = "私の名前は三郎です。";
-	const char* TextMessage2 = "私の年齢は35歳です。";
+	const char* TextMessage2 = "私の年齢は20歳です。";
 	const char* TextMessage5 = "私の好きな食べ物はラーメンです。";
 }
