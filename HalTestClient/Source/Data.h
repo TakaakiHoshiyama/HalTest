b38357bf2715fba31997665e599ec385ad91d416
sue <aaaa>
2024-05-13 19:38:12 +0900

    食べ物をラーメン→寿司に変更

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -2,5 +2,5 @@ namespace Hoshiyama
 {
 	const char* TextMessage1 = "私の名前は三郎です。";
 	const char* TextMessage2 = "私の年齢は20歳です。";
-	const char* TextMessage5 = "私の好きな食べ物はラーメンです。";
+	const char* TextMessage5 = "私の好きな食べ物は寿司です。";
 }
