92dcd1e0e3ae980ce641796c932c96ae0c868862
miyukitsuchiya0712 <beato05maria@gmail.com>
2024-05-13 20:45:43 +0900

    名前の変更

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,7 +1,7 @@
 namespace Hoshiyama
 {
 <<<<<<< HEAD
-	const char* TextMessage1 = "私の名前はカニです。";
+	const char* TextMessage1 = "私の名前は名無しです。";
 	const char* TextMessage2 = "私の年齢は35歳です。";
 	const char* TextMessage5 = "私の好きな食べ物はお寿司です。";
 =======
