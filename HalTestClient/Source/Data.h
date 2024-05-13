95f046a226fb8e1955c0534c5c655e9369963762
34WNINT002\game <kkuni5sity@gmail.com>
2024-05-13 20:31:10 +0900

    名前と好きな食べ物を変更しました。

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -2,14 +2,14 @@ namespace Hoshiyama
 {
 <<<<<<< HEAD
 	// メッセージテキスト
-	const char* TextMessage1 = "私の名前は三郎です。";
+	const char* TextMessage1 = "私の名前はトマトです。";
 =======
 	const char* TextMessage1 = "私の名前は田中です。";
 
 	const char* TextMessage1 = "私の名前はひなです。";
 >>>>>>> origin/NextBranch
 	const char* TextMessage2 = "私の年齢は20歳です。";
-	const char* TextMessage3 = "私の好きな食べ物は焼肉です。";
+	const char* TextMessage3 = "私の好きな食べ物はけばぶです。";
 	const char* TextMessage4 = "私の性別は男性です。";
 }
 
