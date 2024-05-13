c685ca9d42ad9dddaa8e5193db21b1202ec7f6be
THS30834 ショウ　セツショウ <a527745714@gmail.com>
2024-05-13 20:01:34 +0900

    好きなゲームを追加

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -2,5 +2,7 @@ namespace Hoshiyama
 {
 	const char* TextMessage1 = "私の名前は三郎です。";
 	const char* TextMessage2 = "私の年齢は20歳です。";
-	const char* TextMessage3 = "私の好きな食べ物はチーズです。";
+	const char* TextMessage3 = "私の好きなゲームは　　です。";
+	const char* TextMessage5 = "私の好きな食べ物はチーズです。";
+
 }
