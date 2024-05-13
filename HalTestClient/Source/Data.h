6bb0a397a20c25aea59ad9e524a042e52a24bb9b
sue <aaaa>
2024-05-13 20:27:16 +0900

    トッピングの注文を促すメッセージを追加しました

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -2,9 +2,8 @@ namespace Hoshiyama
 {
 	const char* TextMessage1 = "私の名前は三郎です。";
 	const char* TextMessage2 = "私の年齢は35歳です。";
-	const char* TextMessage5 = "私の好きな食べ物はラーメンです。";
-	const char* TextMessage5 = "北海道なら蔵生サブレです。";
-	const char* TextMessage5 = "宮城ならずんだ＆あずきラングドシャです。";
+	const char* TextMessage3 = "私の好きな食べ物はラーメンです。";
+	const char* TextMessage4 = "メンマをトッピングします。続けてトッピングしてください";
 
 }
 
